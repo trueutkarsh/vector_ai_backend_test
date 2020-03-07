5d10cbd588f80619f6992f9b687fa3b65d3ba79e
trueutkarsh <iamutkash@cse.iitb.ac.in>
2020-03-07 11:49:14 +0000

    Basic implementation for DatabaseStorage done

--- writer/databasestorage.py
+++ writer/databasestorage.py
@@ -10,3 +10,63 @@ Create DatabaseStorage class
  -Database Access Tutorial[https://pynative.com/python-postgresql-tutorial/]
 """
 import psycopg2
+from psycopg2 import Error
+
+
+def catch_error(func):
+
+    def _wrapper(*args, **kwargs):
+        try:
+            func(*args, **kwargs)
+            return True
+        except (Exception, Error) as error:
+            # TODO: Log here
+            print(f"Unsuccessfull opertion {error}")
+            return False
+    
+    return _wrapper
+
+
+class DatabaseStorage(object):
+
+
+    def __init__(self, config):
+        self._db_config = config
+        self._cursor = self._get_cursor()
+
+
+    def _get_cursor(self):
+        self._conn = psycopg2.connect(**self._db_config)
+        self._conn.autocommit = True
+        return self._conn.cursor()
+
+    def close(self):
+        self._cursor.close()
+        self._conn.close()
+
+    @catch_error
+    def test_get_all_tables(self):
+        self._cursor.execute(
+            """
+            SELECT table_name FROM information_schema.tables
+            WHERE table_schema = 'public'
+            """
+        )
+        for table in self._cursor.fetchall():
+            self._cursor.execute(f"select * from {table[0]}")
+            print(table[0], self._cursor.fetchall())
+    
+    @catch_error
+    def add_continent(self, name, population):
+        self._cursor.execute("""
+            INSERT INTO continent (name, population)
+            VALUES (%s, %s)
+        """, (name, population))
+        if (self._cursor.count != 1):
+            raise Exception(
+                f"Unsuccess insert value ({name}, {population})"
+            )
+        
+        
+
+
